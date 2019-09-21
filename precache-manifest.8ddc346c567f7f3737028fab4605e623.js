abf7fa71a898b239eb1e36ebd47c0fbc1cf8efed
jpribyl <pribylsnbits@gmail.com>
2019-09-21 17:26:15 -0600

    Updates

---
+++ precache-manifest.8ddc346c567f7f3737028fab4605e623.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62bf6f9cb079d417a59207166b0c2db3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8c0cb020bf3c878913c",
+    "url": "/static/css/main.b439fa94.chunk.css"
+  },
+  {
+    "revision": "a6ae77cb1ced5bfcaa73",
+    "url": "/static/js/2.833112d7.chunk.js"
+  },
+  {
+    "revision": "e8c0cb020bf3c878913c",
+    "url": "/static/js/main.792f6144.chunk.js"
+  },
+  {
+    "revision": "70ccf05882dbc153f5f7",
+    "url": "/static/js/runtime~main.07c5e372.js"
+  },
+  {
+    "revision": "f8f776bd723e2de8a28213e8282469db",
+    "url": "/static/media/pn_logo_blue_small.f8f776bd.png"
+  }
+]);
